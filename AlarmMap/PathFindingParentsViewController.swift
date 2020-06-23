67e8ab2bf5ebd472d03f8a5ca63e71f777950242
Kloong1 <dkdlsmsm98@hanyang.ac.kr>
2020-06-23 14:27:31 +0900

    이동탭 뷰 추가

---
+++ AlarmMap/PathFindingParentsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PathFindingParentsViewController.swift
+//  AlarmMap
+//
+//  Created by 김요환 on 2020/06/23.
+//  Copyright © 2020 AalrmMapCompany. All rights reserved.
+//
+
+import UIKit
+
+class PathFindingParentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

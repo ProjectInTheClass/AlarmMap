721161520cc831cc8cdd28d6df2301522c76b3cf
Kloong1 <dkdlsmsm98@hanyang.ac.kr>
2020-06-24 01:00:58 +0900

    경로 검색 결과 셀 수정중

---
+++ AlarmMap/RoutePreviewBarView.swift
@@ -0,0 +1,21 @@
+//
+//  RoutePreviewBarView.swift
+//  AlarmMap
+//
+//  Created by 김요환 on 2020/06/24.
+//  Copyright © 2020 AalrmMapCompany. All rights reserved.
+//
+
+import UIKit
+
+class RoutePreviewBarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
